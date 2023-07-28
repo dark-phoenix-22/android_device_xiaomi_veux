e1a1a00b98e91316d3a98080d26c893c3efcf724
SIDDHANT <88608943+dark-phoenix-22@users.noreply.github.com>
2023-07-29 02:32:37 +0530

    Update and rename blaze_veux.mk to legion_veux.mk

--- legion_veux.mk
+++ legion_veux.mk
@@ -18,14 +18,14 @@ TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
 # Inherit some common blaze
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, vendor/legion/config/common_full_phone.mk)
 
-PRODUCT_NAME := blaze_veux
+PRODUCT_NAME := legion_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X4 Pro 5G
-BLAZE_MAINTAINER := SIDDHANT(dark-phoenix-22)
+LEGION_MAINTAINER := SIDDHANT(dark-phoenix-22)
 WITH_GAPPS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
